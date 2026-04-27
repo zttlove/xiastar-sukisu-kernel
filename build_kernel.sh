3bde8f92a9b7b0a7d416aa0ab49b737ac61a4aaf
zttlove <115808960+zttlove@users.noreply.github.com>
2026-04-27 17:32:31 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -34,6 +34,7 @@ fi
 # Shallow clone of Proton Clang to minimize network traffic
 if [ ! -d "${TOOLCHAIN_DIR}" ]; then
     echo "  -> Cloning Clang..."
+   mkdir -p "${TOOLCHAIN_DIR}"  # 关键：创建目录，父目录不存在也能自动创建
    wget -qO- https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20241222-release/Clang-20.0.0git-20241222.tar.gz | tar -xzf - -C "${TOOLCHAIN_DIR}"
 fi
 
