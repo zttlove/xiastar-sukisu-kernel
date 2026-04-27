65e4a12642b690d79c36d60a37073d62fcb4391a
zttlove <115808960+zttlove@users.noreply.github.com>
2026-04-27 16:39:03 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -40,7 +40,7 @@ fi
 # Clone the AnyKernel3 template packager
 if [ ! -d "${ANYKERNEL_DIR}" ]; then
     echo "  -> Cloning AnyKernel3..."
-    git clone --depth=1 https://github.com/osm0sis/AnyKernel3.git "${ANYKERNEL_DIR}"
+    wget -qO- https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20241222-release/Clang-20.0.0git-20241222.tar.gz | tar -xzf - -C "${ANYKERNEL_DIR}"
 fi
 
 echo "[2/6] Exporting environment variables (Kbuild Compiler Directives)..."
