62ca6b160e166d48a4184647e5cd7383ed19d8d3
zttlove <115808960+zttlove@users.noreply.github.com>
2026-04-29 08:49:02 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -28,7 +28,7 @@ echo "[1/6] Cloning repositories and toolchain..."
 # Clone the kernel source tree
 if [ ! -d "${KERNEL_DIR}" ]; then
     echo "  -> Cloning kernel_xiaomi_mars (branch: twelve)..."
-    git clone --depth=1 https://github.com/palazik/kernel_xiaomi_mars.git -b twelve "${KERNEL_DIR}"
+    git clone --depth=1 https://github.com/zttlove/kernel_xiaomi_mars.git -b twelve "${KERNEL_DIR}"
 fi
 
 # Shallow clone of Proton Clang to minimize network traffic
